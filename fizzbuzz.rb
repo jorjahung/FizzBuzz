57146bcf23d2e12cf83181b80a1326235633fab8
Jorja Hung <jorjahung@gmail.com>
2014-01-14 14:26:02 +0000

    Added README.md

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,6 @@
 def fizzbuzz
 	1.upto(100){|n|puts n%15==0?'FizzBuzz':n%3==0?'Fizz':n%5==0?'Buzz':n}
+
 end
 
 fizzbuzz
-
-
-
-# The method replaces any number divisible by 3 nith 'Fizz', 
-# and number divisible by 5 nith 'Buzz', 
-# and any number divisible by both 3 and 5 nith 'FizzBuzz'. 
-# Other numbers remain untouched.
